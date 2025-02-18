9f67d2767dd25ef2ea6ac0c413d993e85da3c17c
Lorenzo Ingle <beast102za@gmail.com>
2025-02-18 23:36:03 +0000

    updated

--- Game.cs
+++ Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Media;
 
 namespace DungeonExplorer
@@ -7,19 +8,37 @@ namespace DungeonExplorer
     {
         private Player player;
         private Room currentRoom;
+        private Random random;
+        private string[] itemList;
+        private int itemNum;
+        private string item;
 
-        public Game()
+        public Game(string[] items)
         {
-            // Initialize the game with one room and one player
-
+            Console.Write("Enter your name: ");
+            player = new Player(Console.ReadLine(), 100);
+            currentRoom = new Room();
+            random = new Random();
+            itemList = items;
         }
+
         public void Start()
         {
             // Change the playing logic into true and populate the while loop
-            bool playing = false;
+            bool playing = true;
             while (playing)
             {
-                // Code your playing logic here
+                itemNum = random.Next(0, itemList.Length);
+                item = itemList[itemNum];
+                currentRoom = new Room(item);
+                Console.WriteLine(currentRoom.GetDescription());
+                Console.Write("Would you like to pick up the item yes/no : ");
+                string answer = Console.ReadLine();
+                if (answer.ToLower() == "yes")
+                {
+                    player.PickUpItem(item);
+                    Console.WriteLine($"your inventory contains: {player.InventoryContents()}");
+                }
             }
         }
     }
