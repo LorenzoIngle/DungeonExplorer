30df460ffd06e83a5a8ddcb34ba04d492bd7d118
Lorenzo Ingle <beast102za@gmail.com>
2025-02-22 10:47:41 +0000

    commit

--- Game.cs
+++ Game.cs
@@ -7,6 +7,11 @@ namespace DungeonExplorer
     {
         private Player player;
         private Room currentRoom;
+        private Random random;
+        private string[] itemList;
+        private int itemNum;
+        private string item;
+        private string action;
 
         public Game()
         {
@@ -19,7 +24,17 @@ namespace DungeonExplorer
             bool playing = false;
             while (playing)
             {
-                // Code your playing logic here
+                itemNum = random.Next(0, itemList.Length);
+                item = itemList[itemNum];
+                currentRoom = new Room(item);
+                Console.WriteLine(currentRoom.GetDescription());
+                Console.Write("Would you like to pick up the item yes/no : ");
+                string answer = Console.ReadLine();
+                if (answer.ToLower() == "yes")
+                {
+                    player.PickUpItem(item);
+                    Console.WriteLine($"your inventory contains: {player.InventoryContents()}");
+                }
             }
         }
     }
--- Player.cs
+++ Player.cs
@@ -1,25 +1,69 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace DungeonExplorer
 {
     public class Player
     {
-        public string Name { get; private set; }
-        public int Health { get; private set; }
+        private string _name;
+        private int _health;
         private List<string> inventory = new List<string>();
+
+        public string Name
+        { 
+            get { return _name; } 
+            set 
+            {
+                _name = string.IsNullOrEmpty(value) ? "noName" : value;
+            }
+        }
+
+        public int Health
+        {
+            get { return _health; }
+            set 
+            {
+                if (value < 0)
+                {
+                    _health = 0;
+                }
+                else if (value > 100)
+                {
+                    _health = 100;
+                }
+                else
+                {
+                    _health = value;
+                }
+            }
+        }
 
         public Player(string name, int health) 
         {
             Name = name;
             Health = health;
         }
+
         public void PickUpItem(string item)
         {
-
+            if (inventory.Count < 1)
+            {
+                inventory.Add(item);
+            }
+            else
+            {
+                Console.WriteLine("Inventory full, could not pick up item");
+            }
         }
+
         public string InventoryContents()
         {
             return string.Join(", ", inventory);
+        }
+
+        public void CurrentStatus()
+        {
+            Console.WriteLine($"{Health} HP, your inventory contains: {InventoryContents()}");
         }
     }
 }
